3d71c24d268331bb7a095ddb18adb1994ba99747
jitendrapurohit <jitendra@fuzion.co.nz>
2022-11-12 17:24:47 +0530

    remove unwanted code

--- CRM/Zapier/Page/Participant.php
+++ CRM/Zapier/Page/Participant.php
@@ -28,18 +28,6 @@ class CRM_Zapier_Page_Participant {
         'participant_source' => 'created from zapier',
         'participant_fee_amount' => '100',
       ];
-      // $participant = [
-      //   'id' => '134',
-      //   'contact_id' => 'John Doe',
-      //   'contact_first_name' => 'John',
-      //   'contact_last_name' => 'Doe',
-      //   'contact_email' => 'john.doe@example.com',
-      //   'event_id' => 'Fall Fundraiser Dinner',
-      //   'status_id' => 'Registered',
-      //   'role_id' => 'Volunteer',
-      //   'source' => 'created from zapier',
-      //   'fee_amount' => '100',
-      // ];
     }
     return [$participant];
   }
